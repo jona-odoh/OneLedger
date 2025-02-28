dcaf341596783cdd72aafe63a148a3a6de286234
JONATHAN ODOH <jonathanodoh3140@gmail.com>
2025-02-28 17:39:35 +0100

    Main layout with sidebar, navbar, footer

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>OneLedger</title>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{asset('assets/css/app.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/bundles/bootstrap-social/bootstrap-social.css')}}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
+    <!-- Custom style CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
+    <link rel='shortcut icon' type='image/x-icon' href='{{asset('assets/img/favicon.ico')}}' />
+</head>
+
+<body>
+<div class="loader"></div>
+<div id="app">
+    <div class="main-wrapper main-wrapper-1">
+        @include('layouts._partials.navbar')
+        @include('layouts._partials.sidebar')
+
+        @yield('content')
+
+        @include('layouts._partials.settingSidebar')
+        @include('layouts._partials.footer')
+    </div>
+
+</div>
+<!-- General JS Scripts -->
+<script src="{{asset('assets/js/app.min.js')}}"></script>
+<!-- JS Libraies -->
+<script src="{{asset('assets/bundles/apexcharts/apexcharts.min.js')}}"></script>
+
+<!-- Page Specific JS File -->
+<script src="{{asset('assets/js/page/index.js')}}"></script>
+
+<!-- Template JS File -->
+<script src="{{asset('assets/js/scripts.js')}}"></script>
+<!-- Custom JS File -->
+<script src="{{asset('assets/js/custom.js')}}"></script>
+
+</body>
+
+
+</html>

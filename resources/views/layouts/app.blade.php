07bb699e39fb36c8f0852a2f0c1cab551e7073e6
JONATHAN ODOH <jonathanodoh3140@gmail.com>
2025-03-02 16:00:19 +0100

    Main layout with dynamic sidebar/header/navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>OneLedger</title>
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{asset('assets/css/app.min.css')}}">
@@ -35,8 +36,9 @@
 <script src="{{asset('assets/js/app.min.js')}}"></script>
 <!-- JS Libraies -->
 <script src="{{asset('assets/bundles/apexcharts/apexcharts.min.js')}}"></script>
-
+<script src="{{asset('assets/bundles/sweetalert/sweetalert.min.js')}}"></script>
 <!-- Page Specific JS File -->
+<script src="{{asset('assets/js/page/sweetalert.js')}}"></script>
 <script src="{{asset('assets/js/page/index.js')}}"></script>
 
 <!-- Template JS File -->
